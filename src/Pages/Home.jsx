e68c79dc61b60446b777b3a6e72feca400566134
Muhammad Rashid Hussain <89989802+Rashidhussain547@users.noreply.github.com>
2022-03-27 18:28:13 +0500

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import cardimg1 from "../assets/images/c-1.jpg";
-import cardimg2 from "../assets/images/c-2.jpg";
-import cardimg3 from "../assets/images/c-3.jpg";
+import cardimg1 from "../assets/images/S-1.jpg";
+import cardimg2 from "../assets/images/S-2.jpg";
+import cardimg3 from "../assets/images/S-6.jpg";
 import About from "../Components/About/About";
 import { ParallaxProvider } from "react-scroll-parallax";
 import "./Home.scss";
@@ -26,14 +26,15 @@ const Home = () => {
               <div className="card col-lg-4 col-md-6 col-sm-12">
                 <img src={cardimg1} className="card-img-top" />
                 <div className="card-body">
-                  <h5>Two Different Roads</h5>
+                  <h5>Electrical</h5>
                   <p>
-                    The web design industry has been undergoingrege tremendous
-                    changes to meeteo the demand of users all over to have more
-                    access to con...
+                    Our state-of-art electrical calibration laboratory is
+                    designed to cater all your calibration needs for electrical
+                    instruments such as Multimeter, Ammeter, Voltmeter, Clamp
+                    meter, Insulation tester etc...
                   </p>
                   <span>
-                    <a href="#!">
+                    <a href="/Electric">
                       Read more <i class="fas fa-angle-right"></i>
                     </a>
                   </span>
@@ -42,14 +43,15 @@ const Home = () => {
               <div className="card col-lg-4 col-md-6 col-sm-12">
                 <img src={cardimg2} className="card-img-top" />
                 <div className="card-body">
-                  <h5>Electric Wave Motion</h5>
+                  <h5>Torque</h5>
                   <p>
-                    The web design industry has been undergoingrege tremendous
-                    changes to meeteo the demand of users all over to have more
-                    access to con...
+                    Technical Calibration Laboratory is an authorized
+                    calibration and service center for Snap-on Torque tools.
+                    TechCal has a well-developed facility for calibration of
+                    torque tools such as Torque Wrenches...
                   </p>
                   <span>
-                    <a href="#!">
+                    <a href="/Torque">
                       Read more <i class="fas fa-angle-right"></i>
                     </a>
                   </span>
@@ -58,14 +60,15 @@ const Home = () => {
               <div className="card col-lg-4 col-md-6 col-sm-12">
                 <img src={cardimg3} className="card-img-top" />
                 <div className="card-body">
-                  <h5>Lasting Impressions</h5>
+                  <h5>NDT equipment</h5>
                   <p>
-                    The web design industry has been undergoingrege tremendous
-                    changes to meeteo the demand of users all over to have more
-                    access to con...
+                    Here at TechCal, we are providing calibration services for
+                    NDT equipment/tools. In order to carry out non-destructive
+                    testing; you also need to ensure that the test equipment
+                    like thickness gauges, magnetic yokes etc...
                   </p>
                   <span>
-                    <a href="#!">
+                    <a href="/NDT">
                       Read more <i class="fas fa-angle-right"></i>
                     </a>
                   </span>
